4565d553f0e75460a732702935a7275b21efacd2
Emmanuel Sunga <emmanuelsunga325@gmail.com>
2026-01-23 11:05:27 +0800

    added a feature for release of envelope

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -70,6 +70,9 @@ class DashboardController extends Controller
                 'is_signed' => !$current,
             ]);
 
+            // Reload the relationship to get fresh data after the update
+            $requestModel->load('documents');
+
             // Check if all documents are signed
             $allSigned = $requestModel->documents->every(fn($doc) => $doc->pivot->is_signed);
 
